3da975c9e6dbde8f9aef5ffcf229bb976fc787f8
desak29 <desak29@icloud.com>
2016-03-18 16:17:34 -0400

    MemoryGame 2.1
    -added monster images
    -added back and front of each card via javascript
    -added grid sizing options
    
    Left To do:
    reformat code so that it looks animated
    after victory show transition to animation
    after victory record win
    add time window to complete matching game
    add own pictures to the memory game
    perhaps pivot app into puzzle app

--- scripts.js
+++ scripts.js
@@ -1,65 +1,69 @@
-
-
 var moves = 0;
 var wins = 0;
 var games = 0;
+var cards = [
+
+"<img src='img/monsters-01.jpeg'>",
+"<img src='img/monsters-02.jpeg'>",
+"<img src='img/monsters-03.jpeg'>",
+"<img src='img/monsters-04.jpeg'>",
+"<img src='img/monsters-05.jpeg'>",
+"<img src='img/monsters-06.jpeg'>",
+"<img src='img/monsters-07.jpeg'>",
+"<img src='img/monsters-08.jpeg'>",
+"<img src='img/monsters-09.jpeg'>",
+"<img src='img/monsters-11.jpeg'>",
+"<img src='img/monsters-13.jpeg'>",
+"<img src='img/monsters-14.jpeg'>",
+"<img src='img/monsters-15.jpeg'>",
+"<img src='img/monsters-16.jpeg'>" 
+];
+
 var gridSize;
-var rowSize;
 var gameTiles;
 var gridArray;
-var cards = [
-    "<img src='img/monsters/monsters-01.png>",
-    "<img src='img/monsters/monsters-02.png>",
-    "<img src='img/monsters/monsters-03.png>",
-    "<img src='img/monsters/monsters-04.png>",
-    "<img src='img/monsters/monsters-05.png>",
-    "<img src='img/monsters/monsters-06.png>",
-    "<img src='img/monsters/monsters-07.png>",
-    "<img src='img/monsters/monsters-08.png>",
-    "<img src='img/monsters/monsters-09.png>",
-    "<img src='img/monsters/monsters-10.png>",
-    "<img src='img/monsters/monsters-11.png>",
-    "<img src='img/monsters/monsters-12.png>",
-    "<img src='img/monsters/monsters-13.png>",
-    "<img src='img/monsters/monsters-14.png>",
-    "<img src='img/monsters/monsters-15.png>",
-    "<img src='img/monsters/monsters-16.png>",
-];
+var rowSize;
+
 $(document).ready(function() {
-    $('input').click(function() {
+
+    $("input").click(function() {
         var diff = $(this).val();
-        if (diff == 'easy') {
-            rowsize = 5;
+        if (diff == "easy"){
+            rowSize = 5;
             gridSize = rowSize * 2;
-
-        } else if (diff == 'medium') {
-            rowsize = 5;
+        }else if(diff == "medium"){
+            rowSize = 5;
             gridSize = rowSize * 4;
-
-        } else if (diff == 'hard') {
-            rowsize = 7;
+        }else if(diff == "hard"){
+            rowSize = 7;
             gridSize = rowSize * 4;
-
         }
-        $('#button-bucket').toggle();
-        gameTiles = cards.slice(0, (gridSize / 2));
+        $("#button-bucket").toggle();
+        gameTiles = cards.slice(0, gridSize/2);
         gridArray = $.merge(gameTiles, gameTiles);
-        // takes game tiles and concatanates game tiles into one array gridArray
-        console.log(gridArray);
-        // shuffle here
-        for (i = 0; i < gridArray.length; i++) {
-            var html = '<div class="mg-tile">';
-            html += '<div class="mg-tile-inner unmatched flipped"></div>';
-            html += '<div class="mg-tile-outside"></div>';
-            '<div class="mg-tile-inside"></div>' + gridArray[i] + '</div>';
-            html += '</div>';
-            html += '</div>';
-            $('#mg-contents').append(html);
-            //    append html of pictures to the mg contents 
-            // dont want to override html 
-            // you can keep adding to this thing
 
+
+        //shuffle here
+        //place here
+
+        for (var i = 0; i < gridArray.length; i++){
+            var html = '<div class ="mg-tile">';
+                    html += '<div class="mg-tile-inner unmatched flipped">';
+                        html += '<div class="mg-tile-outside"></div>';
+                        html += "<div class='mg-tile-inside'>" + gridArray[i] + '</div>';
+                    html += '</div>';
+                html += '</div>';
+                $("#mg-contents").append(html);
 
         }
+        setTimeout(function(){
+            $('.mg-tile-inner').removeClass('flipped')
+        },2000);
+        $('.mg-tile').click
+
+        
     });
-})
+
+
+
+});
